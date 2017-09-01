b556edd7a1b395c3f679f116554c5c480fed91ff
Vlad04 <vladiir.rod96@gmail.com>
2017-09-01 13:16:28 -0600

    it works!

--- ns-2.35/homework_2.sh
+++ ns-2.35/homework_2.sh
@@ -2,13 +2,18 @@
 # This script helps to run ns2 multiple times
 num_times=7
 packet_size=64
-for (( i=0; i<=$num_times; i++ ))
+counter=2
+for (( i=1; i<=$num_times; i++ ))
 do
-echo "Running: $i of $num_times with size $packet_size"
- packet_size=packet_size*2
- echo "$packet_size"
+ echo "Running: $i of $num_times with size $packet_size"
+ echo "actual size: $packet_size"
+  ns MCS_8001_example1.tcl $packet_size
+ packet_size=$(($packet_size * $counter))
  echo "-------------------------"
  # run simulator
- ns MCS_8001_example1.tcl $packet_size
+
+ 
+
+
  
 done
